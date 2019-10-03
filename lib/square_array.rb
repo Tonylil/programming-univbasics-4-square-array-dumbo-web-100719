d764b948de25031d1efbdf026739183feb98a9be
FanZhong Zeng <fanzhongzeng78@gmail.com>
2019-10-03 21:09:59 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,5 +2,8 @@ def square_array(array)
   newArray = []
   i = 0
   while i < array.length() do
-    newArray.push(array[i] * )
+    newArray.push(array[i] ** 2)
+    i++
+  end
+  return newArray
 end
